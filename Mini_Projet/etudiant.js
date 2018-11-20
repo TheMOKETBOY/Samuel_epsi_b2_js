d705834bb714adeb17fb9da14066b102ab856664
TheMOKETBOY <tatelvilrus.samuel@gmail.com>
2018-11-20 10:08:40 +0100

    Init command

--- Mini_Projet/etudiant.js
+++
@@ -1,9 +0,0 @@
-class etudiant{
-
-		constructor	(nom, prenom, note){
-			this.nom = nom;
-			this.prenom = prenom;
-			this.note = note;
-
-		}
-}
--- Mini_Projet/storage.js
+++
@@ -1,1 +0,0 @@
-localStorage.setItem('dude', 'Samuel Vilrus');
